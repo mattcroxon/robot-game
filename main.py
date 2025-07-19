ba30f22cfd039a7a39622613eba1de2c5a1332f1
Matt Croxon <mattcrox@gmail.com>
2025-07-19 15:22:53 +0100

    Added the input file, and the file parsing method

--- main.py
+++ main.py
@@ -119,6 +119,30 @@ class Robot():
         
     def __str__(self) -> str:
         return "Robot ID = " + self.id
+       
+def parseIntput(file):
+    f = open(file)
+    gridSize = f.readline().rstrip().split(' ')
+    instructions = {} 
+    instructions['gridSize'] = {} 
+    instructions['gridSize']['x'] = gridSize[0]
+    instructions['gridSize']['y'] = gridSize[1]
+    instructions['robotGuidance'] = []
+    while True:
+        robotStartingPosition = f.readline()
+        robotInstructionsString = f.readline()
+        startingPositionFormatted = robotStartingPosition.rstrip().split(' ')
+        if len(startingPositionFormatted) != 3: 
+            break
+        robotInstructions = {}
+        robotInstructions['startingPosition'] = {}
+        robotInstructions['startingPosition']['x'] = startingPositionFormatted[0]
+        robotInstructions['startingPosition']['y'] = startingPositionFormatted[1]
+        robotInstructions['startingPosition']['direction'] = startingPositionFormatted[2]
+        robotInstructions['instructions'] = list(robotInstructionsString.rstrip())
+        instructions['robotGuidance'].append(robotInstructions)
+        if not robotInstructionsString: break  # EOF
+    return instructions 
         
 def main():
     grid = Grid(10, 10)
@@ -154,4 +178,4 @@ def main():
     
     print(grid.toString())
 
-main()
+print(parseIntput("input.txt"))
