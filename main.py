28e4181f130c1048574a7ecc3d42a9bff507fb03
Matt Croxon <mattcrox@gmail.com>
2025-07-24 16:13:10 +0100

    Added a rotation method, changed the Enum to more simply manage direction & introduced the processing of the file

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 
 
 from enum import Enum
+import uuid
 
 class Orientation(Enum):
-    NORTH = 'N'
-    SOUTH = 'S'
-    EAST = 'E'
-    WEST = 'W'
+    N = 0
+    S = 180
+    E = 90 
+    W = 270 
 
 class Grid():
     def __init__(self, x, y) -> None:
@@ -14,39 +15,41 @@ class Grid():
         self.robots = {}
         self.deadRobots = []
         self.robotScents = []
-        self.maxWidth = x
-        self.maxHeight = y
+        self.maxWidth = int(x)
+        self.maxHeight = int(y)
         # print("I am in init, {y}", (self.matrix))
        
     def addRobot(self, robot):
-        self.robots[robot.getId()] = (0, 0, False) # x, y, robotScent
+        initialPosition = robot.getCoordinates()
+        self.robots[robot.getId()] = (initialPosition['x'], initialPosition['y'], False) # x, y, robotScent
         
     def removeRobot(self, robot):
         self.robots.pop(robot.getId())
         self.deadRobots.append(robot)
         print(f"{robot.getId()} is dead and has been removed")
         
-    def getTargetPosition(self, currentPosition, orientation, steps): 
-        if orientation == 'N':
-            if  currentPosition[1] - steps < 0: 
+    def getTargetPosition(self, currentPosition, direction, steps): 
+        print("In get target position. Current position is ", currentPosition)
+        if direction == Orientation.N:
+            if  currentPosition[1] - int(steps) < 0: 
                 return (currentPosition[0], 0, True)
             else: 
-                return (currentPosition[0], currentPosition[1] - steps, False)
-        elif orientation == 'S':
-            if  currentPosition[1] + steps > self.maxHeight: 
+                return (currentPosition[0], currentPosition[1] - int(steps), False)
+        elif direction == Orientation.S:
+            if  currentPosition[1] + int(steps) > self.maxHeight: 
                 return (currentPosition[0], self.maxHeight, True)
             else: 
-                return (currentPosition[0], currentPosition[1] + steps, False)
-        elif orientation == 'E':
-            if  currentPosition[0] - steps < 0: 
+                return (currentPosition[0], currentPosition[1] + int(steps), False)
+        elif direction == Orientation.E:
+            if  currentPosition[0] - int(steps) < 0: 
                 return (currentPosition[0], 0, True)
             else: 
-                return (currentPosition[0], currentPosition[0] - steps, False)
-        elif orientation == 'W':
-            if  currentPosition[0] + steps > self.maxWidth: 
+                return (currentPosition[0], currentPosition[0] - int(steps), False)
+        elif direction == Orientation.W:
+            if  currentPosition[0] + int(steps) > self.maxWidth: 
                 return (currentPosition[0], self.maxWidth, True)
             else: 
-                return (currentPosition[0], currentPosition[0] + steps, False)
+                return (currentPosition[0], currentPosition[0] + int(steps), False)
         else:
             raise Exception("Wrong orientation")
 
@@ -61,8 +64,10 @@ class Grid():
                 print("An existing scent was found. Robot is not moving further")
                 self.robots[robotToMove.getId()] = targetPosition    
             else:
+                robotToMove.kill()
                 self.removeRobot(robotToMove)
                 self.robotScents.append(targetPosition)
+                print("Robot has died: ", robotToMove)
         else:
             self.robots[robotToMove.getId()] = targetPosition
             
@@ -74,11 +79,12 @@ class Grid():
         print("============================")
 
 class Robot(): 
-    def __init__(self, id, grid) -> None:
+    def __init__(self, id, grid, initialX, initialY, initialDirection) -> None:
         self.id = id
-        self.x = 0
-        self.y = 0 
-        self.orientation = 360
+        self.alive = True
+        self.x = initialX
+        self.y = initialY
+        self.direction = Orientation[initialDirection]
         self.grid = grid
         self.grid.addRobot(self)
         pass
@@ -88,39 +94,54 @@ class Robot():
 
     def getId(self):
         return self.id
-
-    def getDirection(self): 
-        match self.orientation:
-            case 0:
-                return 'N'
-            case 90:
-                return 'E'
-            case 180:
-                return 'S'
-            case 270:
-                return 'W'
-            case _:
-                raise Exception("Direction unknown")
-             
-    def move(self, command): 
-        # print("I am in the move method")
+    
+    def kill(self):
+        self.alive = False
+    
+    def rotate(self, command):
+        print("Initial direction is", self.direction)
         match command:
             case "L":
-                self.orientation -= 90 % 360
-                print(f"Robot {self.id} - new orientiation is ", self.orientation)
+                newDirection =  (self.direction.value - 90) % 360
             case "R": 
-                self.orientation += 90 % 360
-                print(f"Robot {self.id} - new orientiation is ", self.orientation)
-            case "F":
-                print(f"Robot {self.id} - current direction is", self.getDirection())
-                self.grid.moveRobot(self, self.getDirection(), 1)
+                newDirection =  (self.direction.value + 90) % 360        
             case _: 
-                raise Exception("Command not known")
+                raise Exception("Rotation command not known")
+        # print(f"Robot {self.id} - move output is ", newDirection)
+        self.direction = Orientation(newDirection)
+    
+    def getCoordinates(self):
+        return { 
+                'x': int(self.x), 
+                'y': int(self.y), 
+                'direction': self.direction
+        }
+         
+    def move(self, command): 
+        # print("I am in the move method")
+        # print('Current position ', self.getCoordinates())
+        if self.alive == False:
+            print("Robot " + str(self.id) + " is dead and won't be moving")
+        else:
+            match command:
+                case "L":
+                    # self.orientation = (self.orientation - 90) % 360
+                    self.rotate(command)
+                    print(f"Robot {self.id} moved left - It is now at ", self.getCoordinates())
+                case "R":
+                    # self.orientation = (self.orientation - 90) % 360
+                    self.rotate(command)
+                    print(f"Robot {self.id} moved right - It is now at ", self.getCoordinates())
+                case "F":
+                    self.grid.moveRobot(self, self.direction, 1)
+                    print(f"Robot {self.id} moved forward. It is now at ", self.getCoordinates())
+                case _: 
+                    raise Exception("Command not known")
         
     def __str__(self) -> str:
-        return "Robot ID = " + self.id
+        return "Robot ID = " + str(self.id) + " [x = " + str(self.x) + ", y = " + str(self.y) + ", direction = " + str(self.direction.name) + "]"
        
-def parseIntput(file):
+def parseInput(file):
     f = open(file)
     gridSize = f.readline().rstrip().split(' ')
     instructions = {} 
@@ -143,39 +164,26 @@ def parseIntput(file):
         instructions['robotGuidance'].append(robotInstructions)
         if not robotInstructionsString: break  # EOF
     return instructions 
-        
-def main():
-    grid = Grid(10, 10)
-    robot1 = Robot('Robot1', grid)
-    robot2 = Robot('Robot2', grid)
-    robot1.move('L')
-    robot1.move('L')
-    robot1.move('F')
-    robot1.move('F')
-    robot1.move('F')
-    robot1.move('F')
-    robot1.move('F')
-    robot1.move('F')
-    robot1.move('F')
-    robot1.move('F')
-    robot1.move('F')
-    robot1.move('F')
-    robot1.move('F')
-    
-    robot2.move('L')
-    robot2.move('L')
-    robot2.move('F')
-    robot2.move('F')
-    robot2.move('F')
-    robot2.move('F')
-    robot2.move('F')
-    robot2.move('F')
-    robot2.move('F')
-    robot2.move('F')
-    robot2.move('F')
-    robot2.move('F')
-    robot2.move('F')
-    
-    print(grid.toString())
 
-print(parseIntput("input.txt"))
+def processInput(input):
+    # print(input)
+    grid = Grid(input['gridSize']['x'], input['gridSize']['y'])
+    for robotInstruction in input['robotGuidance']:
+        print(robotInstruction)
+        robot = Robot(uuid.uuid4(), grid, 
+                      robotInstruction['startingPosition']['x'], 
+                      robotInstruction['startingPosition']['y'], 
+                      robotInstruction['startingPosition']['direction'])
+        print(robot)
+        for individualMove in robotInstruction['instructions']:
+            print(individualMove)
+            robot.move(individualMove)
+            
+        
+    print(grid.robots)
+    pass
+    
+def main():
+    processInput(parseInput('input.txt'))
+
+main()
